b670800cd1ab08c55c264e25a16469f1b7f0f97e
br3w0r <daniildirun123@gmail.com>
2023-02-25 18:18:56 +0300

    fix migration

--- migrations/20230129191946_v1-device-sensor.sql
+++ migrations/20230129191946_v1-device-sensor.sql
@@ -1,6 +1,6 @@
 create table device_sensor (
 	device_id integer not null references device(id),
-	sensor_table_name text
+	sensor_table_name text not null
 );
 
 create unique index device_sensor_idx on device_sensor(device_id, sensor_table_name);
