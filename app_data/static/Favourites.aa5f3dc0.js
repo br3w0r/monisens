216bfde9b4316e81cef6887fa734983e46f2bde6
br3w0r <daniildirun123@gmail.com>
2023-05-14 23:44:20 +0300

    fix frontend

---
+++ app_data/static/Favourites.aa5f3dc0.js
@@ -0,0 +1,1 @@
+import{d as e,u as s,c as o,o as r,_ as t}from"./index.7512e9c8.js";const a=e({__name:"Favourites",setup(n){return s(),(i,u)=>(r(),o("div",null,"This is a Favourites page"))}}),c=t(a,[["__file","/Users/daniildirun/dev/monisens-frontend/src/views/Favourites.vue"]]);export{c as default};
