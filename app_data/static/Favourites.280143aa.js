70021ce6a9f03ae68c5ec3f14826de602f357505
br3w0r <daniildirun123@gmail.com>
2024-05-05 15:43:30 +0400

    fix front

--- app_data/static/Favourites.280143aa.js
+++
@@ -1,1 +0,0 @@
-import{d as e,u as a,c as o,o as s}from"./index.8f32404b.js";const c=e({__name:"Favourites",setup(t){return a(),(r,n)=>(s(),o("div",null,"This is a Favourites page"))}});export{c as default};
